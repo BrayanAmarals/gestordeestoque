b3aa7db365921e0f54775367d2b8143a44b191f7
brayanass <proffleandro.linux@gmail.com>
2023-08-21 10:44:56 -0300

    bugFixes 2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/gestor-de-estoque",
+  optimizeDeps: {
+    include: ["react-router-dom"],
+  },
+});
